85e816db2fec1821a2a4aed9a64f4a142e26d393
Eliot Brenner <ebrenner@shutterstock.com>
2014-04-06 18:44:33 -0400

    second commit of Question and MultiplicationQuestion

---
+++ mathsInstructional/mathsInstructional/Fraction.py
@@ -0,0 +1,31 @@
+from fractions import Fraction
+
+"""
+A MyFraction consists of a python fraction, which is its
+lowest form representation, and a multiple, which
+allows us to represent the fraction in a non-reduced form
+"""
+
+
+class MyFraction:
+    def __init__(self, *args, **kwargs):
+        d = 1
+        n = args[0]
+        if len(args > 1):
+            d = args[1]
+        m = 1
+        if len(args > 2):
+            m = args[2]
+        self.reducedForm = Fraction(n, d)
+        self.multiple = m
+
+    def setMultiple(self, multiple):
+        self.multiple = multiple
+
+    def getMultiple(self):
+        return self.multiple
+
+    def __str__(self):
+        return "(%s,%s)" % (self.mutliple*self.reducedForm.numerator,
+                            self.mutliple*self.reducedForm.denominator)
+
--- mathsInstructional/mathsInstructional/MultiplicationQuestion.py
+++ mathsInstructional/mathsInstructional/MultiplicationQuestion.py
@@ -1,4 +1,5 @@
 from Question import Question
+from Fraction import MyFraction
 
 """
 A multiplication question asks for the product of first and second
@@ -10,3 +11,23 @@ class MultiplicationQuestion(Question):
         self.super()
         self.first = first
         self.second = second
+        self.reversed = False
+        self.answer = MyFraction(first*second)
+
+    def reverse(self):
+        if self.reversed:
+            self.reversed = False
+        else:
+            self.reversed = True
+
+    def __str__(self):
+        first, second = self.first, self.second
+        if self.reversed:
+            second, first = first, second
+            return "%schr(158)%s" % (first, second)
+
+    def __repr__(self):
+        first, second = self.first, self.second
+        if self.reversed:
+            second, first = first, second
+            return r'%s\times%s' % (first, second)
